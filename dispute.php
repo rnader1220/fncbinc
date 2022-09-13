6f6f0ffb9a0819edb568c3e33874350d0087ff11
Richard Nader <rnader@dyn-it.com>
2022-09-13 13:13:05 +0000

    done

--- dispute.php
+++ dispute.php
@@ -1,46 +1,61 @@
 <?php
+ 
+require_once('./src/PHPMailer.php');
+require_once('./src/Exception.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors', True);
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
 $success = false;
 
 if($_POST) {
     $message = '';
     $valid=true;
-    if(!$_POST['fname']) {
+    if(!isset($_POST['fname'])) {
         $message.= "First Name Required<br/>";
         $valid=false;
     }
-    if(!$_POST['lname']) {
+    if(!isset($_POST['lname'])) {
         $message.= "Last Name Required<br/>";
         $valid=false;
     }
-    if(!$_POST['addr1']) {
+    if(!isset($_POST['addr1'])) {
         $message.= "Address Line 1 Required<br/>";
         $valid=false;
     }
-    if(!$_POST['city']) {
+    if(!isset($_POST['city'])) {
         $message.= "Address City Required<br/>";
         $valid=false;
     }
-    if(!$_POST['state']) {
+    if(!isset($_POST['state'])) {
         $message.= "Address State Required<br/>";
         $valid=false;
     }
-    if(!$_POST['zipcode']) {
+    if(!isset($_POST['zipcode'])) {
         $message.= "Address Zip Code Required<br/>";
         $valid=false;
     }
-    if(!$_POST['phone']) {
+    if(!isset($_POST['phone'])) {
         $message.= "Phone Number Required<br/>";
         $valid=false;
     }
-    if(!$_POST['email']) {
+    if(!isset($_POST['email'])) {
         $message.= "Email Address Required<br/>";
         $valid=false;
     }
-    if(!$_POST['refnumber']) {
+    if(!isset($_POST['refnumber'])) {
         $message.= "Reference Number Required<br/>";
         $valid=false;
     }
-    if(!$_POST['reason']) {
+    if(!isset($_POST['reason'])) {
         $message.= "Dispute Reason Required<br/>";
         $valid=false;
     }
@@ -49,12 +64,14 @@ if($_POST) {
         $message.= "Please Add Required Fields";
         $success = false;
     } else {
+
         $to = "correspondence@fncbinc.com"; // this is your Email address
+        //$to = 'rnader@dyn-it.com'; // Test Email Address
         $from = $_POST['email']; // this is the sender's Email address
         $first_name = $_POST['fname'];
         $last_name = $_POST['lname'];
         $subject = "Dispute Form submission";
-        $message = "Dispute Form Data: \n\n" .
+        $textmessage = "Dispute Form Data: \n\n" .
         "First Name: {$_POST['fname']} \n".
         "Last Name: {$_POST['lname']} \n\n".
         "Address: \n" . 
@@ -70,15 +87,42 @@ if($_POST) {
     
         $headers = "From:" . $from;
 
-        mail($to,$subject,$message,$headers);
+        /* new method  */
+            $pemail = new PHPMailer();
+            $pemail->SetFrom($from, "{$first_name} {$last_name}"); //Name is optional
+            $pemail->Subject   = $subject;
+            $pemail->Body      = $textmessage;
+            $pemail->AddAddress( $to );
+            if(isset($_SESSION['documents'])) {
+                foreach ($_SESSION['documents'] as $index => $document) {
+                    $pemail->AddAttachment( $document['pathname'] , $document['filename']);
+                    $message.= "Document Attached: " . $document['filename'] . "<br/>";
+                }
+            }
 
-        $message = "Your Dispute has been Recieved";
+            $pemail->Send();
+
+            if(isset($_SESSION['documents'])) {
+                foreach ($_SESSION['documents'] as $index => $document) {
+                    unlink($document['pathname']);
+                }
+            }
+
+
+
+
+        
+
+        $message .= "Your Dispute has been Received<br/>";
         $success = true;
+        session_destroy();
     }
 } else {
     $message = "If you would like to dispute the debt that we have contacted you about, ". 
     "please call us toll free at <a href='tel:800-824-6191'>(800) 824-6191</a> or submit the following information. " . 
     "All information provided serves as express written consent to contact you in order to resolve this matter." . 
+    "<br><br>Attach any supporting documents below the form BEFORE submitting dispute form.".
+
     "<br><br>By submitting the form below, you certify that you are being truthful and that all information provided is accurate.";
 }
 
@@ -94,12 +138,21 @@ if($_POST) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Security-Policy" content="frame-ancestors none">
-    <title>First National Collections Bureau - Prototype</title>
+    <title>First National Collections Bureau - Dispute Form</title>
     <link rel="icon" type="image/icon" href="/images/fncb.ico">
     <meta name="description"
         content="First National Collections Bureau is a nationally licensed full service accounts receivable management firm. Helping consumers resolve debts for over 35 years." />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/app.css?v=1">
+    <link href="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <style>
+        #doc_uploader .input-group div, 
+        #doc_uploader .input-group a {
+            padding-top: 18px;  
+        }
+    </style>
 
 </head>
 
@@ -119,6 +172,8 @@ if($_POST) {
 <?php if ($success) { ?>
 <?php } else { ?>
         <div class='section-two'>
+            <div class = 'row'>
+                <div class = 'col-lg-12'>
             <form method='POST'>
                 <div class="row">
 
@@ -127,7 +182,7 @@ if($_POST) {
                             <label for="fname" class="control-label">First Name</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="fname" name="fname"
-                                    <?php if($_POST['fname']) {echo "value='{$_POST['fname']}'";} ?> 
+                                    <?php if(isset($_POST['fname'])) {echo "value='{$_POST['fname']}'";} ?> 
                                 >
                             </div>
                         </div>
@@ -138,7 +193,7 @@ if($_POST) {
                             <label for="lname" class="control-label">Last Name</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="lname" name="lname"
-                                    <?php if($_POST['lname']) {echo "value='{$_POST['lname']}'";} ?>
+                                    <?php if(isset($_POST['lname'])) {echo "value='{$_POST['lname']}'";} ?>
                                 >
                             </div>
                         </div>
@@ -150,7 +205,7 @@ if($_POST) {
                             <label for="addr1" class="control-label">Address</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="addr1" name="addr1"
-                                    <?php if($_POST['addr1']) {echo "value='{$_POST['addr1']}'";} ?>
+                                    <?php if(isset($_POST['addr1'])) {echo "value='{$_POST['addr1']}'";} ?>
                                 >
                             </div>
                         </div>
@@ -159,7 +214,7 @@ if($_POST) {
                         <div class="form-group">
                             <div class="input-group">
                                 <input class="form-control" type="text" id="addr2" name="addr2"
-                                    <?php if($_POST['addr2']) {echo "value='{$_POST['addr2']}'";} ?>
+                                    <?php if(isset($_POST['addr2'])) {echo "value='{$_POST['addr2']}'";} ?>
                                 >
                             </div>
                         </div>
@@ -169,7 +224,7 @@ if($_POST) {
                             <label for="city" class="control-label">City</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="city" name="city"
-                                    <?php if($_POST['city']) {echo "value='{$_POST['city']}'";} ?>
+                                    <?php if(isset($_POST['city'])) {echo "value='{$_POST['city']}'";} ?>
                                 >
                             </div>
                         </div>
@@ -179,7 +234,7 @@ if($_POST) {
                             <label for="state" class="control-label">ST</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="state" name="state"
-                                    <?php if($_POST['state']) {echo "value='{$_POST['state']}'";} ?>
+                                    <?php if(isset($_POST['state'])) {echo "value='{$_POST['state']}'";} ?>
                                 >
                             </div>
                         </div>
@@ -189,7 +244,7 @@ if($_POST) {
                             <label for="zipcode" class="control-label">Zip Code</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="zipcode" name="zipcode"
-                                    <?php if($_POST['zipcode']) {echo "value='{$_POST['zipcode']}'";} ?>
+                                    <?php if(isset($_POST['zipcode'])) {echo "value='{$_POST['zipcode']}'";} ?>
                                 >
                             </div>
                         </div>
@@ -201,7 +256,7 @@ if($_POST) {
                             <label for="phone" class="control-label">Phone Number</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="phone" name="phone"
-                                    <?php if($_POST['phone']) {echo "value='{$_POST['phone']}'";} ?>
+                                    <?php if(isset($_POST['phone'])) {echo "value='{$_POST['phone']}'";} ?>
                                 >
                             </div>
                         </div>
@@ -211,7 +266,7 @@ if($_POST) {
                             <label for="email" class="control-label">Email Address</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="email" name="email"
-                                    <?php if($_POST['email']) {echo "value='{$_POST['email']}'";} ?>
+                                    <?php if(isset($_POST['email'])) {echo "value='{$_POST['email']}'";} ?>
                                     >
                             </div>
                         </div>
@@ -222,7 +277,7 @@ if($_POST) {
                             <label for="refnumber" class="control-label">Reference Number</label>
                             <div class="input-group">
                                 <input class="form-control" type="text" id="refnumber" name="refnumber"
-                                    <?php if($_POST['refnumber']) {echo "value='{$_POST['refnumber']}'";} ?>
+                                    <?php if(isset($_POST['refnumber'])) {echo "value='{$_POST['refnumber']}'";} ?>
                                 >
                             </div>
                         </div>
@@ -235,12 +290,12 @@ if($_POST) {
                             <label for="reason" class="control-label">Dispute Reason</label>
                             <select class="form-control" name="reason" id="sel_reason">
                                 <option selected disabled value=''>-- select one --</option>
-                                <option value="Fraud">Fraud</option>
-                                <option value="Verification of Debt">Verification of Debt</option>
-                                <option value="Dispute Balance">Dispute Balance</option>
-                                <option value="Paid Prior">Paid Prior</option>
-                                <option value="Returned Merchandise">Returned Merchandise</option>
-                                <option value="Substantiation of Debt">Substantiation of Debt (New York Only)</option>
+                                <option <?php if(isset($_POST['reason'])) { if($_POST['reason'] == 'Fraud') echo 'checked';} ?> value="Fraud">Fraud</option>
+                                <option <?php if(isset($_POST['reason'])) { if($_POST['reason'] == 'Verification of Debt') echo 'checked';} ?> value="Verification of Debt">Verification of Debt</option>
+                                <option <?php if(isset($_POST['reason'])) { if($_POST['reason'] == 'Dispute Balance') echo 'checked';} ?> value="Dispute Balance">Dispute Balance</option>
+                                <option <?php if(isset($_POST['reason'])) { if($_POST['reason'] == 'Paid Prior') echo 'checked';} ?> value="Paid Prior">Paid Prior</option>
+                                <option <?php if(isset($_POST['reason'])) { if($_POST['reason'] == 'Paid Prior') echo 'checked';} ?> value="Paid Prior">Returned Merchandise</option>
+                                <option <?php if(isset($_POST['reason'])) { if($_POST['reason'] == 'Substantiation of Debt') echo 'checked';} ?> value="Substantiation of Debt">Substantiation of Debt (New York Only)</option>
                             </select>
                         </div>
                     </div>
@@ -248,7 +303,7 @@ if($_POST) {
                     <div class="col-lg-12" id="detail_div">
                         <div class="form-group">
                             <label for="detail" class="control-label">Description of Dispute</label>
-                            <textarea class="form-control" id="detail" name="detail"><?php if($_POST['detail']) {echo "{$_POST['detail']}";} ?></textarea>
+                            <textarea class="form-control" id="detail" name="detail"><?php if(isset($_POST['detail'])) {echo "{$_POST['detail']}";} ?></textarea>
                         </div>
                     </div>
                 </div>
@@ -259,8 +314,25 @@ if($_POST) {
                     </div>
                 </div>
             </form>
+                </div>
+            <div class = 'col-lg-12'>
+            <span class="file-name">Upload files before submitting form!</span>
+            <form id='doc_uploader' enctype="multipart/form-data">
+                <div class="upload-wrapper">
+                <label for="file-upload">Browse</label>
+        
+                <input type="file" id="upload_document" name="uploadedFile">
+                </div>
+            </form>
+            </div>
         </div>
-<?php } ?>
+    </div>
+    <script>
+
+    </script>
+
+
+  <?php } ?>
 
     </div>
 
@@ -268,6 +340,11 @@ if($_POST) {
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/js/plugins/piexif.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/js/plugins/sortable.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/js/fileinput.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/kartik-v/bootstrap-fileinput@5.2.2/themes/fas/theme.min.js"></script>
+
     <script>
         function toggler(divId) {
             $("#" + divId).toggle();
@@ -277,6 +354,15 @@ if($_POST) {
             $.each(includes, function () {
             var file = 'views/' + $(this).data('include') + '.html'
             $(this).load(file)
+
+
+            $("#upload_document").fileinput({
+                bsVersion: "5.x",
+                theme: "fas",
+                maxFileCount: 4,
+                uploadUrl: '/dispute_upload.php'
+            });
+
         })
 })
     </script>
