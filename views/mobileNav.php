9ab0b7896b831fca4ccc51c6eb37cbdd097a00ff
Richard Nader <rnader@dyn-it.com>
2023-05-19 14:55:41 +0000

    images

--- views/mobileNav.php
+++ views/mobileNav.php
@@ -3,11 +3,11 @@
     <div class="row gx-0">
         <div class="col-6 menu-item"><a href="/">Home</a></div>
         <!-- div class="col-6 menu-item"><a href="/remove-number">Remove Number</a></div -->
-        <div class="col-6 menu-item"><a href="/contact">Contact</a></div>
+        <div class="col-6 menu-item"><a href="/contact.php">Contact</a></div>
         <div class="col-6 menu-item"><a href="/dispute.php">Dispute</a></div>
         <div class="col-6 menu-item"><a href="/custcare.php">Customer Care</a></div>
         <div class="col-6 menu-item"><a href="/complaint.php">Complaint</a></div>
-        <div class="col-6 menu-item"><a href="/resources.html">Resources</a></div>
+        <div class="col-6 menu-item"><a href="/resources.php">Resources</a></div>
         <!-- need to point to actual login portal-->
         <div class="col-6 menu-item"><a target='_blank'
                 href="<?=$server_link ?>">Login</a></div>
