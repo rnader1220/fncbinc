749a5e4bea41903bd621f1bf020c536f4f89698f
Richard Nader <rnader@dyn-it.com>
2023-05-20 11:32:18 +0000

    restore mobile use

--- views/header.php
+++ views/header.php
@@ -3,40 +3,63 @@
     //$server_link =  '/server-maintenance.php';
 ?>
 <header>
-<div class="container">
-    <div class="row">
-        <div class="col-4 col-sm-2 col-md-2 col-lg-2 desktop">
-            <a href="/" class="logo" title="First National Collections Bureau, Inc. FNCBNV">First National Collections
-                Bureau, Inc. FNCBNV</a>
-        </div>
-        <div class="col-4 col-sm-10 col-md-10 col-lg-10 desktop ">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <!-- li><a href="/remove-number.php">Remove Number</a></li -->
-                <li><a href="/contact.php">Contact</a></li>
-
-                <li><a href="/dispute.php">Dispute</a></li>
-                <li><a href="/custcare.php">Customer Care</a></li>
-                <li><a href="/complaint.php">Complaint</a></li>
-                <li><a href="/resources.php">Resources</a></li>
-
-
+    <div class="mobile ">
+        <div>
+<a href="#" class="mobile-menu" onclick="$('#mobileNav').toggle();"><i class="fa-regular fa-bars"></i></a>
+</div>
+        <div class="mobile-nav" id="mobileNav" style='display:none'>
+                <a href="/">Home</a><br>
+                <!-- div class="col-6 menu-item"><a href="/remove-number">Remove Number</a></div -->
+                <a href="/contact.php">Contact</a><br>
+                <a href="/dispute.php">Dispute</a><br>
+                <a href="/custcare.php">Customer Care</a><br>
+                <a href="/complaint.php">Complaint</a><br>
+                <a href="/resources.php">Resources</a><br>
                 <!-- need to point to actual login portal-->
-                <li><a target='_blank' href="<?=$server_link ?>">Review Your Account</a></li>
-
-                <!-- need to point to actual login portal-->
-                <li><a target='_blank' href="<?=$server_link ?>"
-                        class="button normal">Make Payment</a></li>
-            </ul>
-        </div>
-        <div class="col-8 col-sm-10 col-md-10 col-lg-10 mobile ">
-            <ul>
-                <!-- need to point to actual login portal-->
-                <li><a target='_blank' href="<?=$server_link ?>"
-                        class="button normal">Make Payment</a></li>
-                <li><a href="#" class="mobile-menu" onclick="toggler('mobileNav');"><i class="icon menu"></i></a></li>
-            </ul>
+                <a target='_blank' href="<?=$server_link ?>">Login</a><br>
+                <a target='_blank' href="<?=$server_link ?>" style='font-weight:bold; font-size:1.25em' >Make Payment</a>
         </div>
     </div>
-</div>
+    <div class="top-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    Call with questions or to pay by phone: <a href="tel:800-824-6191">(800)
+                        824-6191</a>&nbsp;&nbsp;|&nbsp;&nbsp;TTY/TTD service: Dial 711&nbsp;&nbsp;|&nbsp;&nbsp;Se habla
+                    español
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4 col-sm-2 col-md-2 col-lg-2 desktop">
+                <a href="/" class="logo" title="First National Collections Bureau, Inc. FNCBNV">First National
+                    Collections
+                    Bureau, Inc. FNCBNV</a>
+            </div>
+            <div class="col-4 col-sm-10 col-md-10 col-lg-10 desktop ">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <!-- li><a href="/remove-number.php">Remove Number</a></li -->
+                    <li><a href="/contact.php">Contact</a></li>
+
+                    <li><a href="/dispute.php">Dispute</a></li>
+                    <li><a href="/custcare.php">Customer Care</a></li>
+                    <li><a href="/complaint.php">Complaint</a></li>
+                    <li><a href="/resources.php">Resources</a></li>
+
+
+                    <!-- need to point to actual login portal-->
+                    <li><a target='_blank' href="<?=$server_link ?>">Review Your Account</a></li>
+
+                    <!-- need to point to actual login portal-->
+                    <li><a target='_blank' href="<?=$server_link ?>" class="button normal">Make Payment</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
 </header>
