d1cb9842568083b386814d4434ea145d210fb352
Richard Nader <rnader@dyn-it.com>
2024-07-04 11:44:35 -0400

    implement new forms tool design

---
+++ complaint2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+$success = false;
+
+?>
+
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>First National Collections Bureau - Complaint Form</title>
+    <link rel="icon" type="image/icon" href="/images/fncb.ico">
+    <meta name="description"
+        content="First National Collections Bureau is a nationally licensed full service accounts receivable management firm. Helping consumers resolve debts for over 35 years." />
+    <?php include 'views/styles.php' ?>
+
+</head>
+
+<body>
+<?php include 'views/topbar.php' ?>
+    <?php include 'views/header.php' ?>
+    <?php include 'views/mobileNav.php' ?>
+
+    <div class="container">
+        <div class='section-one'>
+        <div id="wufoo-q1pxqwfh011n41o"> 
+            Fill out my <a href="https://fncbnv.wufoo.com/forms/q1pxqwfh011n41o">online form</a>. 
+        </div> 
+        <script type="text/javascript"> var q1pxqwfh011n41o; (function(d, t) { var s = d.createElement(t), options = { 'userName':'fncbnv', 'formHash':'q1pxqwfh011n41o', 'autoResize':true, 'height':'944', 'async':true, 'host':'wufoo.com', 'header':'show', 'ssl':true }; s.src = ('https:' == d.location.protocol ?'https://':'http://') + 'secure.wufoo.com/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() { var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { q1pxqwfh011n41o = new WufooForm(); q1pxqwfh011n41o.initialize(options); q1pxqwfh011n41o.display(); } catch (e) { } }; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script'); </script> 
+    
+        </div>
+
+    </div>
+    <?php include 'views/footer.php' ?>
+</body>
+<?php include 'views/scripts.php' ?>
+</html>
---
+++ dispute2.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>First National Collections Bureau - Contact Us</title>
+    <link rel="icon" type="image/icon" href="/images/fncb.ico">
+    <meta name="description"
+        content="First National Collections Bureau is a nationally licensed full service accounts receivable management firm. Helping consumers resolve debts for over 35 years." />
+
+    <?php include 'views/styles.php' ?>
+
+</head>
+
+<body>
+    <?php include 'views/topbar.php' ?>
+    <?php include 'views/header.php' ?>
+    <?php include 'views/mobileNav.php' ?>
+
+    <div class="container">
+    <div class='section-one'>
+        <div id="wufoo-m3nu4el0o7f0w4"> 
+            Fill out my <a href="https://fncbnv.wufoo.com/forms/m3nu4el0o7f0w4">online form</a>. 
+        </div>
+        <script type="text/javascript"> var m3nu4el0o7f0w4; (function(d, t) { var s = d.createElement(t), options = { 'userName':'fncbnv', 'formHash':'m3nu4el0o7f0w4', 'autoResize':true, 'height':'1170', 'async':true, 'host':'wufoo.com', 'header':'show', 'ssl':true }; s.src = ('https:' == d.location.protocol ?'https://':'http://') + 'secure.wufoo.com/scripts/embed/form.js'; s.onload = s.onreadystatechange = function() { var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return; try { m3nu4el0o7f0w4 = new WufooForm(); m3nu4el0o7f0w4.initialize(options); m3nu4el0o7f0w4.display(); } catch (e) { } }; var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr); })(document, 'script'); </script>
+
+            </div>
+    </div>
+
+
+    <?php include 'views/footer.php' ?>
+</body>
+<?php include 'views/scripts.php' ?>
+
+</html>
+
