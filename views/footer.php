d323eacac5243f06383a81613dedef60358b393e
Richard Nader <rnader@dyn-it.com>
2025-01-04 11:25:38 -0500

    upd

--- views/footer.php
+++ views/footer.php
@@ -27,8 +27,8 @@
                             <p>This communication is from a debt collector.<br>This is an attempt to collect a debt and any
                                 information<br>obtained will be used for that purpose.</p>
                                 <p style='font-size:0.80em;'>NMLS #:934310
-                                <br>Compliance Manager NMLS #:941563
                                 <br>NV Collection Agency License #: CAD11256
+                                <br>Compliance Manager NMLS #:941563
                                 <br>Compliance Manager Certificate Identification #: CM100001</p>
                         </div>
 
